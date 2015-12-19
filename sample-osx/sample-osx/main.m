200971e491cfff58de9bd79fe1c724518c78f493
Hugo Marques <hugodesmarques@gmail.com>
2015-12-19 13:55:23 -0300

    Exercises

---
+++ sample-osx/sample-osx/main.m
@@ -0,0 +1,16 @@
+    //
+//  main.m
+//  sample-osx
+//
+//  Created by Hugo Marques on 12/16/15.
+//  Copyright © 2015 Hugo Marques. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+
+    printf("Hello, World!\n");
+
+    return 0;
+}
