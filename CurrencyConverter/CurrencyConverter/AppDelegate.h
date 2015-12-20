bc473556d4985e7ee320d4f110cf9104729d8911
Hugo Marques <hugodesmarques@gmail.com>
2015-12-20 10:47:34 -0300

    Currency converter init

---
+++ CurrencyConverter/CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Hugo Marques on 12/20/15.
+//  Copyright © 2015 Hugo Marques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
