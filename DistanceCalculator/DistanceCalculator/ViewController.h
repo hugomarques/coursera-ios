c093987d72f47c848ca123dc8a4a986d60aa3d41
Hugo Marques <hugodesmarques@gmail.com>
2015-12-24 11:39:37 -0300

    Setup distance calculator

---
+++ DistanceCalculator/DistanceCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DistanceCalculator
+//
+//  Created by Hugo Marques on 12/24/15.
+//  Copyright © 2015 Hugo Marques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
